6680247177af762e9d18cde39240944e81357583
eu mesmo <luiz_sena@hotmail.com>
2024-01-26 19:16:57 -0300

    Primeira questão

--- Services/RandomService.cs
+++ Services/RandomService.cs
@@ -5,11 +5,15 @@
 		int seed;
 		public RandomService()
 		{
-			seed = Guid.NewGuid().GetHashCode();
-		}
-		public int GetRandom()
+            // o problema é que o valor da seed era criado quando a controller criava a variável _randomService,
+			// fazendo com que o valor dela não mudasse. Para ser criada um novo valor, é necessário que o GetHashCode()
+			// seja invocado a cada vez que for requisitado a criação de um novo número aleatorio.
+        }
+        public int GetRandom()
 		{
-			return new Random(seed).Next(100);
+            seed = Guid.NewGuid().GetHashCode();
+
+            return new Random(seed).Next(100);
 		}
 
 	}
